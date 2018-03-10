9c4f79d141cadcf20394fa91c6e71808d37496cd
Dandigit <37099129+Dandigit@users.noreply.github.com>
2018-03-11 08:37:08 +1000

    Update syncTest.py

--- syncTest.py
+++ syncTest.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python
 
-import sheetupdate
+import pyrebase
 import datetime
-from random import randint
 from time import sleep
 
-def testsheet():
-    spreadsheetId = '1HxIF-7LGQI-OtNyNIYU9ao0CKPfzFW6eGyurC6qvmBI'
-    rangeName = 'A1:D'
-    values = {'values':[['Time','Date','Name','ID'],]}
-  
-    sheetupdate.update_authenticate(spreadsheetId, rangeName, values)
-  
-try:
-    while True:
-        for i in range(2):
-            currentTime = '{:%H:%M:%S}'.format(datetime.datetime.now())
-            currentDate = '{:%dT:%m:%Y}'.format(datetime.datetime.now())
-            c = "This would be the card ID, but this is just a test."
-            d = "This would be the student's name, but this is just a test."
-            values = {'values':[[currentTime, currentDate, c, d],]}
-            rangeName = 'A'+ str(i) +':D'
-            sheetupdate.update_authenticate(spreadsheetId, rangeName, values)
-            print("Success!")
+# Enter your Firebase credentials in here
+config = {
+  "apiKey": "Enter Firebase apiKey here",
+  "authDomain": "projectId.firebaseapp.com",
+  "databaseURL": "https://databaseName.firebaseio.com",
+  "storageBucket": "projectId.appspot.com"
+}
 
-if __name__ == '__main__':
-    testsheet()
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
+db.child("users").child("Morty")
+data = {"name": "Mortimer 'Morty' Smith"}
+db.child("users").push(data)
