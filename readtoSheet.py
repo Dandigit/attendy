3afd536d81054a4eafd942e27fc9824368643e53
Dandigit <37099129+Dandigit@users.noreply.github.com>
2018-03-10 17:06:37 +1000

    8 spaces indentation

--- readtoSheet.py
+++ readtoSheet.py
@@ -10,24 +10,24 @@ from time import sleep
 reader = SimpleMFRC522.SimpleMFRC522()
 
 def testsheet():
-  spreadsheetId = '1HxIF-7LGQI-OtNyNIYU9ao0CKPfzFW6eGyurC6qvmBI'
-  rangeName = 'A1:D'
-  values = {'values':[['Time','Date','Name','ID'],]}
+        spreadsheetId = '1HxIF-7LGQI-OtNyNIYU9ao0CKPfzFW6eGyurC6qvmBI'
+        rangeName = 'A1:D'
+        values = {'values':[['Time','Date','Name','ID'],]}
   
-  sheetupdate.update_authenticate(spreadsheetId, rangeName, values)
+        sheetupdate.update_authenticate(spreadsheetId, rangeName, values)
   
 try:
-  id, text = reader.read()
-  currentTime = '{:%H:%M:%S}'.format(datetime.datetime.now())
-  currentDate = '{:%dT:%m:%Y}'.format(datetime.datetime.now())
-  c = id
-  d = text
-  values = {'values':[[currentTime, currentDate, c, d],]}
-  rangeName = 'A'+ str(i) + ':D'
-  sheetupdate.update_authenticate(spreadsheetId, rangeName, values)
+        id, text = reader.read()
+        currentTime = '{:%H:%M:%S}'.format(datetime.datetime.now())
+        currentDate = '{:%dT:%m:%Y}'.format(datetime.datetime.now())
+        c = id
+        d = text
+        values = {'values':[[currentTime, currentDate, c, d],]}
+        rangeName = 'A'+ str(i) + ':D'
+        sheetupdate.update_authenticate(spreadsheetId, rangeName, values)
 finally:
-  GPIO.cleanup()
+        GPIO.cleanup()
   
 if __name__ == '__main__':
-  testsheet()
+        testsheet()
   
