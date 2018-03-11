a17bc94e3a326823e3cd4e35aa1ec84dd2ff8c4f
Dandigit <37099129+Dandigit@users.noreply.github.com>
2018-03-11 11:13:43 +1000

    Update write.py

--- write.py
+++ write.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
+import sys
+sys.path.append('libraries/SimpleMFRC522.py')
 import SimpleMFRC522
 
 reader = SimpleMFRC522.SimpleMFRC522()
@@ -9,6 +11,6 @@ try:
         text = raw_input('Enter data to write: ')
         print("Please place your tag on the reader")
         reader.write(text)
-        print("Write successful")
+        print("Write successful!")
 finally:
         GPIO.cleanup()
