7a34c05e55fdc421997891d6e987b90fd534b722
Dandigit <37099129+Dandigit@users.noreply.github.com>
2018-03-11 16:02:17 +1000

    Update syncTest.py

--- syncTest.py
+++ syncTest.py
@@ -1,3 +1,4 @@
+import os
 import logging
 from sheetsync import Sheet, ia_credentials_helper
 # Turn on logging so you can see what sheetsync is doing.
@@ -10,11 +11,11 @@ CLIENT_SECRET = 'Enter client secret here'
 creds = ia_credentials_helper(CLIENT_ID, CLIENT_SECRET,
                               credentials_cache_file='cred_cache.json')
 
-data = { "Kermit": {"Color" : "Green", "Performer" : "Jim Henson"},
-         "Miss Piggy" : {"Color" : "Pink", "Performer" : "Frank Oz"} }
+data = { "Row 1": {"Test" : "Succeeded", "Test" : "Succeeded"},
+         "Row 2" : {"Test" : "Succeeded", "Test" : "Succeeded"} }
 
 # Find or create a spreadsheet, then inject data.
-target = Sheet(credentials=creds, document_name="Sync Test")
+target = Sheet(credentials=creds, document_name="Attendy Sync Test")
 target.inject(data)
-print "Spreadsheet created here: %s" % target.document_href
-os.system('mpg123 -q success.mp3')
+print "Spreadsheet created/appended here: %s" % target.document_href
+os.system('mpg123 -q success.mp3 &')
