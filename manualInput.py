da8b6913cd48a032e4d337c86011430bd319af7d
Dandigit <37099129+Dandigit@users.noreply.github.com>
2018-03-11 15:55:16 +1000

    Update manualInput.py

--- manualInput.py
+++ manualInput.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import RPi.GPIO as GPIO
 import datetime
 import os
 import logging
@@ -9,8 +8,8 @@ from sheetsync import Sheet, ia_credentials_helper
 # Normally logging would be turned on, but we don't want to confuse the user
 
 # Enter your OAUTH2 credentials here:
-CLIENT_ID = '946386881583-q0jl6r8280p56o0ma4qph2157tmp1hs2.apps.googleusercontent.com'
-CLIENT_SECRET = 'rhniAHN0Rz_cs_AGB81TrkWX'
+CLIENT_ID = 'Enter client ID here'
+CLIENT_SECRET = 'Enter client secret here'
 creds = ia_credentials_helper(CLIENT_ID, CLIENT_SECRET,
                               credentials_cache_file='cred_cache.json')
 
@@ -26,4 +25,4 @@ data = { name: {"Time" : currentTime, "Date" : currentDate}}
 target = Sheet(credentials=creds, document_name="Attendy")
 target.inject(data)
 print "Written to spreadsheet: %s" % target.document_href
-os.system('success.mp3')
+os.system('mpg123 -q success.mp3 &')
