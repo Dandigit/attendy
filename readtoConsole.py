a26389c7cb0f09900d87a3acbe7518a54bded55e
Dandigit <37099129+Dandigit@users.noreply.github.com>
2018-03-11 15:56:37 +1000

    Update readtoConsole.py

--- readtoConsole.py
+++ readtoConsole.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
+import os
 import sys
 sys.path.append('libraries/SimpleMFRC522.py')
 import SimpleMFRC522
@@ -11,5 +12,6 @@ try:
     id, text = reader.read()
     print("Card ID: ", id)
     print("Data: ", text)
+    os.system('mpg123 -q success.mp3 &')
 finally:
     GPIO.cleanup()
