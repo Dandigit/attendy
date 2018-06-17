d86180f00e14f5d222798f5446496135d3a2d4cf
Dandigit <37099129+Dandigit@users.noreply.github.com>
2018-06-17 17:39:29 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-exec > /dev/null 2>&1
 
 # This is the Attendy Bash installer.
 # It will install Attendy and its dependencies.
@@ -11,51 +10,51 @@ echo "Installing Attendy..."
 tput sc
 echo "[       ]"
 
-sudo apt-get install python2.7-dev
+sudo apt-get install python2.7-dev > /dev/null 2>&1
 
 tput rc
 tput ed
-echo "[$'\u0965'      ]"
+echo "[\u0965      ]"
 
-git clone https://github.com/lthiery/SPI-Py.git
+git clone https://github.com/lthiery/SPI-Py.git > /dev/null 2>&1
 
 tput rc
 tput ed
-echo "[$'\u0965'$'\u0965'     ]"
+echo "[\u0965\u0965     ]"
 
 cd ~/SPI-Py
-sudo python setup.py install
+sudo python setup.py install > /dev/null 2>&1
 
 tput rc
 tput ed
-echo "[$'\u0965'$'\u0965'$'\u0965'    ]"
+echo "[\u0965\u0965\u0965    ]"
 
 cd ~
 sudo rm -rf SPI-Py
 
-pip install sheetsync
+pip install sheetsync > /dev/null 2>&1
 
 tput rc
 tput ed
-echo "[$'\u0965'$'\u0965'$'\u0965'$'\u0965'   ]"
+echo "[\u0965\u0965\u0965\u0965   ]"
 
-sudo pip install sheetsync
-
-tput rc
-tput ed
-echo "[$'\u0965'$'\u0965'$'\u0965'$'\u0965'$'\u0965'  ]"
-
-sudo pip uninstall gspread
+sudo pip install sheetsync > /dev/null 2>&1
 
 tput rc
 tput ed
-echo "[$'\u0965'$'\u0965'$'\u0965'$'\u0965'$'\u0965'$'\u0965' ]"
+echo "[\u0965\u0965\u0965\u0965\u0965  ]"
 
-sudo pip install --upgrade gspread==0.6.2
+sudo pip uninstall gspread > /dev/null 2>&1
 
 tput rc
 tput ed
-echo "[$'\u0965'$'\u0965'$'\u0965'$'\u0965'$'\u0965'$'\u0965'$'\u0965']"
+echo "[\u0965\u0965\u0965\u0965\u0965\u0965 ]"
+
+sudo pip install --upgrade gspread==0.6.2 > /dev/null 2>&1
+
+tput rc
+tput ed
+echo "[\u0965\u0965\u0965\u0965\u0965\u0965\u0965]"
 
 echo ""
 echo "------------------------- URGENT -------------------------"
